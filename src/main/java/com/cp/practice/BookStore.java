9d12e44d1e581f9e421ab37feb766ceb712c85b3
cpemaratech <cp.sarathe@gmail.com>
2020-01-20 21:30:00 +0400

    book store addition test cases

--- src/main/java/com/cp/practice/BookStore.java
+++ src/main/java/com/cp/practice/BookStore.java
@@ -1,13 +1,25 @@
 package com.cp.practice;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BookStore {
     private List<String> books = new ArrayList<>();
 
     public List<String> books() {
-        return Collections.EMPTY_LIST;
+        return this.books;
+    }
+
+    public void add(String bookName){
+        this.books.add(bookName);
+    }
+    public void add(String... bookName) {
+        Arrays.stream(bookName).forEach(book->books.add(book));
+    }
+
+    public List<String> arrange() {
+        return this.books.stream().sorted().collect(Collectors.toList());
     }
 }
--- src/test/java/com/cp/practice/BookStoreTest.java
+++ src/test/java/com/cp/practice/BookStoreTest.java
@@ -1,29 +1,53 @@
 package com.cp.practice;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Book Store Under Test")
 class BookStoreTest {
+    private BookStore bookStore;
 
     @Test
-    @DisplayName("Store is Empty When No Book Added")
-    void storeIsEmptyWhenNoBookAdded(){
-        BookStore bookStore = new BookStore();
-        List<String> books = bookStore.books();
-        assertTrue(books.isEmpty(),()-> "Book store should be empty");
+    @Disabled
+    void testShouldFail(){
+        fail(()-> "Failed test case");
+    }
+
+    @BeforeEach
+    public void setup(){
+        bookStore = new BookStore();
     }
 
     @Test
-    @DisplayName("Should check even numbers")
-    @RepeatedTest(5)
-    void shouldCheckForEvenNumber(){
-        int num = new Random(1).nextInt();
-        assertTrue(()-> num%2==0,num + " is not an even number");
+    @DisplayName("Empty Book Store when No Books Added")
+    void shouldHaveEmptyBookStoreIfNoBookAdded(){
+        assertTrue(bookStore.books().size()==0);
+    }
+
+    @Test
+    @DisplayName("Add 2 Books to Book Store")
+    void shouldContain2BookWhen2BooksAdded(){
+        bookStore.add("Effective Java" , "AWS Certified Solutions Architect");
+        List<String> books = bookStore.books();
+        assertTrue(books.contains("Effective Java") , ()-> "Effective Java should exist in book store");
+        assertTrue(books.contains("AWS Certified Solutions Architect") , ()-> "AWS Certified Solutions Architect should exist in bookstore");
+        assertTrue(books.size() == 2 , () -> "Book store do not have 2 books in shelf");
+    }
+
+//    As a user, I should be able to arrange my bookshelf based on certain
+//    criteria
+
+    @Test
+    @DisplayName("Arrange Books By Name Lexicographically")
+    void shouldArrangeBooksByNameAndOrder(){
+        bookStore.add("Effective Java" , "AWS Certified Solutions Architect" , "Spring In Action" , "How to Win Friends");
+        List<String> books = bookStore.arrange();
+        List<String> expected = Arrays.asList( "AWS Certified Solutions Architect" , "Effective Java", "Spring In Action" , "How to Win Friends").stream().sorted().collect(Collectors.toList());
+        assertEquals(expected,books);
     }
 }
